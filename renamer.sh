b59cf29ca8ff961bfdeebb387973d6f61aaee8b1
Pranav Sastry <pranava.sri@gmail.com>
2022-01-09 11:17:05 +0530

    Renamed all files with cs_228 or cs228 to regular files

---
+++ renamer.sh
@@ -0,0 +1,10 @@
+for f in *
+do
+	if [[ "$(echo -n "$f" | cut -d "_" -f 1)" == "cs228" ]]
+	then
+		mv "$f" "$(echo -n "$f" | cut -d "_" -f 2-)"
+	elif [[ $(echo -n "$f" | cut -d "_" -f 1-2) == "cs_228" ]]
+	then
+		mv "$f" "$(echo -n "$f" | cut -d "_" -f 3-)"
+	fi
+done
